d8a529beacf50cb1ef1d944ecdc2707d2dc81d68
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2024-08-18 21:09:08 -0500

    feat: add launch darkly

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  experimental: { instrumentationHook: true },
+};
 
 export default config;
